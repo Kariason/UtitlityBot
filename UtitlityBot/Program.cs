fd99083c3cfc9f36c337be20071e69804e907d09
Kariason <147307493+Kariason@users.noreply.github.com>
2024-02-27 19:03:57 +0500

    Добавьте файлы проекта.

---
+++ UtitlityBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using UtitlityBot;
+
+namespace VoiceTexterBot
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services))
+                .UseConsoleLifetime()
+                .Build();
+
+            Console.WriteLine("Starting Service");
+            await host.RunAsync();
+            Console.WriteLine("Service stopped");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("5393213794:AAEh8YHDFUxCiA2ViWAqQ2leX-zPvGcfvCc"));
+            services.AddHostedService<Bot>();
+        }
+    }
+}
+
